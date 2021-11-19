f41d1f24c95ff211603b3ded388a43d7045805c0
Sergey Kharkevich <sergeykharkevich@gmail.com>
2021-11-19 23:48:26 +0100

    Homework1: Sergey Kharkevich

---
+++ Homework1/exam4.py
@@ -0,0 +1,11 @@
+# 4 lines: Fibonacci, tuple assignment
+
+
+parents, babies = (1, 1)
+while babies < 145:
+    print('This generation has {0} babies'.format(babies))
+    parents, babies = (babies, parents + babies)
+
+
+print(parents)
+print(babies)
---
+++ Homework1/exam5.py
@@ -0,0 +1,19 @@
+# 5 lines: Functions
+
+
+# def greet(name):
+#     print('Hello', name)
+#
+#
+# greet('Jack')
+# greet('Jill')
+# greet('Bob')
+
+NAME = input('What is your name?\n')
+
+
+def greet(name):
+    print('Hello', name)
+
+
+greet(NAME)
