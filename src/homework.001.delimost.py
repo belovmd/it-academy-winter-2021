52951cc7d418b099f93c0357961e21ff9b4465e0
JIekceu <alexmashster@gmail.com>
2021-11-20 14:26:40 +0300

    Update homework.001.delimost.py

--- src/homework.001.delimost.py
+++ src/homework.001.delimost.py
@@ -8,22 +8,22 @@
 4. Переписать оператор or через вложенные условные
 операторы.
 '''
-while True :
+while True:
     a = int(input('Введите число: '))
-    if a % 17 == 0 :
+    if a % 17 == 0:
         print('1. ', a, 'делится на 17')
     else:
         print('1. ', a, 'не делится на 17')
-    if a % 2 > 0 :
+    if a % 2 > 0:
         print('2. ого')
     else:
         print('2. ага')
-    if a % 2 > 0 :
+    if a % 2 > 0:
         print('3. ого')   
-    if a % 2 == 0 and a < 0 :
+    if a % 2 == 0 and a < 0:
         print('3. маловато')
-    elif a % 2 == 0 and  a > 0 :
+    elif a % 2 == 0 and  a > 0:
         print('3. нормально')
-    elif a == 0 :
+    elif a == 0:
         print('3. на нет спроса нет')
 
