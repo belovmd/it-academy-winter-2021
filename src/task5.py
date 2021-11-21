60a9027bd87f0927ee19296512103a2b91256c3f
Krotiks <kate.krot@tut.by>
2021-11-21 17:39:12 +0300

    my commit

---
+++ src/task5.py
@@ -0,0 +1,10 @@
+def convert(seconds):
+    days = seconds // (24 * 3600)
+    seconds %= 24 * 3600
+    hours = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+    print(f'{days}:{hours}:{minutes}:{seconds}')
+
+convert(1234565)
