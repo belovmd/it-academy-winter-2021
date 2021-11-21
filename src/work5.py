7bd232d72adb0f9ff46b9a347050509d3cb55856
Krotiks <94705794+Krotiks@users.noreply.github.com>
2021-11-21 19:35:52 +0300

    Update work5.py

--- src/work5.py
+++ src/work5.py
@@ -4,4 +4,4 @@ my_purchase = {
     'banana': 6}
 grocery_bill = sum(prices[fruit] * my_purchase[fruit]
                    for fruit in my_purchase)
-print ('I owe the grocer $%.2f' % grocery_bill)
+print('I owe the grocer $%.2f' % grocery_bill)
