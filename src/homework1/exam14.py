ccb7abff6d72d38eaa6e426aa6e667de1a0a3e65
Okulevich Ivan <kennariuss@gmail.com>
2021-11-21 17:12:36 +0300

    fix some issues in exam14 and exam19

--- src/homework1/exam14.py
+++ src/homework1/exam14.py
@@ -2,10 +2,10 @@
 
 
 def median(pool):
-    '''Statistical median to demonstrate doctest.
+    """Statistical median to demonstrate doctest.
     >>> median([2, 9, 9, 7, 9, 2, 4, 5, 8])
     6 #change to 7 in order to pass the test
-    '''
+    """
     copy = sorted(pool)
     size = len(copy)
     if size % 2 == 1:
