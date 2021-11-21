c277737a8e0da60e9d59134131c84cfdb208108d
Krotiks <94705794+Krotiks@users.noreply.github.com>
2021-11-21 19:35:27 +0300

    Update work4.py

--- src/work4.py
+++ src/work4.py
@@ -9,7 +9,7 @@ This is the second.
 # consecutive lines that either have content or don't.
 for has_chars, frags in groupby(lines, bool):
     if has_chars:
-        print (' '.join(frags))
+        print(' '.join(frags))
 # PRINTS:
 # This is the first paragraph.
 # This is the second.
