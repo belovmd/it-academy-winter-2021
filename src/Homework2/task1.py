b324c774be2b8c54496b7e2d98cb70080447e98d
Varabei Andrei <varabei1393@gmail.com>
2021-11-23 14:50:16 +0300

    add task1

--- src/Homework2/task1.py
+++ src/Homework2/task1.py
@@ -7,8 +7,8 @@ amount_product_N = price_product_N * amount_product
 amount_product_M = price_product_M * amount_product
 i = amount_product_M / 100
 if i < 1:
-    print(amount_product_N, 'рублей', amount_product_M, 'копеек')
+    print(amount_product_N, 'рублей', amount_product_M, 'копейки')
 else:
     amount_product_N = amount_product_N + int(i)
     amount_product_M = amount_product_M % 100
-    print(amount_product_N, 'рублей', amount_product_M, 'копеек')
+    print(amount_product_N, 'рублей', amount_product_M, 'копейки')
