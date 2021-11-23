acea62db7b58a2ed6d43633bc8b9ba4f483e7a95
EugeneBulakh <bulakh.eugene@gmail.com>
2021-11-23 15:05:06 +0300

    Homework1

---
+++ src/homework1/task3.py
@@ -0,0 +1,4 @@
+# 3 lines: For loop, built-in enumerate function, new style formatting
+friends = ['john', 'pat', 'gary', 'michael']
+for i, name in enumerate(friends):
+    print("iteration {iteration} is {name}".format(iteration=i, name=name))
