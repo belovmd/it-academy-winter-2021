68f4062af36ecd25e5a2c827b1670fd8608ac279
Varabei Andrei <varabei1393@gmail.com>
2021-11-23 20:14:10 +0300

    updat8

--- src/Homework2/task1.py
+++ src/Homework2/task1.py
@@ -1,14 +1,14 @@
 # Программа считает общую цену товара в рублях и копейках
 
-price_product_N = int(input('Цена товара, рублей: '))
-price_product_M = int(input('Цена товара, копеек: '))
-amount_product = int(input('Количество товара: '))
-amount_product_N = price_product_N * amount_product
-amount_product_M = price_product_M * amount_product
-i = amount_product_M / 100
-if i < 1:
-    print(amount_product_N, 'рублей', amount_product_M, 'копейки')
+price_rub = int(input('Цена товара, рублей: '))
+price_coin = int(input('Цена товара, копеек: '))
+product_amount = int(input('Количество товара: '))
+total_price_rub = price_rub * product_amount
+total_price_coin = price_coin * product_amount
+converted_to_rub = total_price_coin / 100
+if converted_to_rub < 1:
+    print(total_price_rub, 'рублей', total_price_coin, 'копейки')
 else:
-    amount_product_N = amount_product_N + int(i)
-    amount_product_M = amount_product_M % 100
-    print(amount_product_N, 'рублей', amount_product_M, 'копейки')
+    total_price_rub = total_price_rub + int(converted_to_rub)
+    total_price_coin = total_price_coin % 100
+    print(total_price_rub, 'рублей', total_price_coin, 'копейки')
