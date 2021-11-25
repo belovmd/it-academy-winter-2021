cc8e7da4b9bfec0d5298489c489a916151534c72
Varabei Andrei <varabei1393@gmail.com>
2021-11-25 18:07:26 +0300

    task5  fix eflake8

--- src/Homework2/task5.py
+++ src/Homework2/task5.py
@@ -1,4 +1,4 @@
-#Вывод n-го число Фибоначчи
+# Вывод n-го число Фибоначчи
 
 n = int(input("Введите n число Фибоначи: "))
 fib_1 = 1
@@ -6,7 +6,7 @@ fib_2 = 0
 fib_n = 0
 
 if n > 0:
-    for counter in range(0,n):
+    for counter in range(0, n):
         fib_n = fib_1 + fib_2
         fib_2 = fib_1
         fib_1 = fib_n
@@ -14,7 +14,7 @@ if n > 0:
         fib_n = fib_2
 
 else:
-    for counter in range(n,0):
+    for counter in range(n, 0):
         fib_n = fib_2 - fib_1
         fib_2 = fib_1
         fib_1 = fib_n
