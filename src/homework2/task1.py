daf45cf42b0e09e89d16bc8f0fed91ad30b03612
MarinaIgorevna4 <marinatsykunenko97@gmail.com>
2021-11-26 12:30:40 +0300

    my hw1 v 999

--- src/homework2/task1.py
+++
@@ -1,7 +0,0 @@
-m = int(input('Цена одной вещи, рублей:'))
-n = int(input('копеек:'))
-s = int(input('Количетсво товара:'))
-stoimost = s * (m + n / 100)
-rub = stoimost // 1
-cent = stoimost % 1 * 100
-print('Общая цена', int(rub), 'рублей', int(cent), 'копеек')
