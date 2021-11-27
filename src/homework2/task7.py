185124ea2bb39137a06ff9771c388a5278666e68
Roman Snetkov <romajmg@gmail.com>
2021-11-27 20:02:48 +0300

    hw2_fixes4

--- src/homework2/task7.py
+++ src/homework2/task7.py
@@ -6,11 +6,8 @@ If the sides define a triangle, find its area.
 If not, display an invalid data message.
 """
 
-
 side1, side2, side3 = map(int, input('Input triangle sides space separated: ').split())
-if ((side1 < side2 + side3) and
-    (side2 < side1 + side3) and
-    (side3 < side1 + side2)):
+if (side1 < side2 + side3) and (side2 < side1 + side3) and (side3 < side1 + side2):
     p = (side1 + side2 + side3) / 2
     S = p * (p - side1) * (p - side2) * (p - side3) ** 0.5
     print('It is triangle. Area:', S)
