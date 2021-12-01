bd9181223b195a3d00493dc513d0445a2bbc0e63
KaterinaSelevich <92476413+KaterinaSelevich@users.noreply.github.com>
2021-12-01 17:30:07 +0300

    Delete app.py

--- app.py
+++
@@ -1,4 +0,0 @@
-print('I’m planning to learn git conflicts resolution')
-name = input(‘Please enter your name:’)
-surname = input(‘Please enter your surname:’)
-print(‘Hi, {} {}!’.format(name, surname))
