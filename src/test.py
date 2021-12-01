6691ca7e35a115b48cb2ad188cb1a8d7d23fc811
Alexei <alexbest1515@gmail.com>
2021-12-01 21:08:17 +0300

    clone1

--- src/test.py
+++ src/test.py
@@ -2,3 +2,10 @@ parents, babies = (1, 1)
 while babies < 100:
     print ('This generation has {0} babies'.format(babies))
     parents, babies = (babies, parents + babies)
+
+def greet(name):
+    print ('Hello', name)
+
+greet('Jack')
+greet('Jill')
+greet('Bob')
