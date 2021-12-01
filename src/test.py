ea2c35dcf649952bb33c67b584e9746adaf8fc9c
Alexei <alexbest1515@gmail.com>
2021-12-01 22:12:30 +0300

    clone4

--- src/test.py
+++ src/test.py
@@ -1,9 +1,6 @@
 print('Hello, world!')
-
 name = input('What is your name?\n')
 print('Hi, %s.' % name)
-
 friends = ['john', 'pat', 'gary', 'michael']
 for i, name in enumerate(friends):
     print('iteration {iteration} is{name}'.format(iteration=i, name=name))
-
