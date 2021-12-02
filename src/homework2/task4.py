3d298daa6e2188360dc43cd890ec7f2f385cc8de
Ivan Veremeichik <43482831+NorgeMan@users.noreply.github.com>
2021-12-02 20:55:06 +0300

    Update task4.py

--- src/homework2/task4.py
+++ src/homework2/task4.py
@@ -8,11 +8,9 @@ low_case_letters = 0
 upper_case_letters = 0
 alphabet_string = string.ascii_letters
 for i in range(len(input_string)):
-    if (input_string[i].islower()
-            and input_string[i] in alphabet_string):
+    if (input_string[i].islower() and input_string[i] in alphabet_string):
         low_case_letters += 1
-    if (input_string[i].isupper() 
-            and input_string[i] in alphabet_string):
+    if (input_string[i].isupper() and input_string[i] in alphabet_string):
         upper_case_letters += 1
 print(f"{low_case_letters} маленьких букв в строке:")
 print(f"{upper_case_letters} больших букв в строке:")
