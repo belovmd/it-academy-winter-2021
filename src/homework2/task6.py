0ea0bdf449072214da0eb38b73bda07f75092b0d
Ivan Veremeichik <43482831+NorgeMan@users.noreply.github.com>
2021-12-04 10:39:44 +0300

    Update task6.py

--- src/homework2/task6.py
+++ src/homework2/task6.py
@@ -5,12 +5,12 @@
 Задача требует работать только с числами
 (без конвертации числа в строку или что-нибудь еще)"""
 number = int(input("Введите число: "))
-reverse = 0
+reverse_number = 0
 while number != 0:
     digit = number % 10
-    result = reverse * 10 + digit
+    result = reverse_number * 10 + digit
     number //= 10
-if reverse == number:
+if reverse_number == number:
     print("Palindrome")
 else:
     print("Not a palindrome")
