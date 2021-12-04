82447582cd88f70d4ad5ea0041ea38fb66c392c7
Ivan Veremeichik <43482831+NorgeMan@users.noreply.github.com>
2021-12-04 13:13:42 +0300

    Update task3.py

--- src/homework2/task3.py
+++ src/homework2/task3.py
@@ -1,9 +1,9 @@
 # task 3
 """Вводится строка.
 Требуется удалить из нее повторяющиеся символы и все пробелы."""
-input_string = input("Введите строку: ")
-final_string = ""
-for char in input_string:
-    if char not in final_string and char != " ":
-        final_string += char
-print(final_string)
+str_ = input("Введите строку: ")
+final_str_ = ""
+for char in str_:
+    if char not in final_str_ and char != " ":
+        final_str_ += char
+print(final_str_)
