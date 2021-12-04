6888637be63404ebca5f9bc81878672ce33c23e1
Ivan Veremeichik <43482831+NorgeMan@users.noreply.github.com>
2021-12-04 11:13:57 +0300

    Update task4.py

--- src/homework2/task4.py
+++ src/homework2/task4.py
@@ -7,10 +7,10 @@ input_string = input("Введите текст: ")
 low_case_letters = 0
 upper_case_letters = 0
 alphabet_string = string.ascii_letters
-for i in range(len(input_string)):
-    if (input_string[i].islower() and input_string[i] in alphabet_string):
+for char in range(len(input_string)):
+    if (input_string[char].islower() and input_string[char] in alphabet_string):
         low_case_letters += 1
-    if (input_string[i].isupper() and input_string[i] in alphabet_string):
+    if (input_string[char].isupper() and input_string[char] in alphabet_string):
         upper_case_letters += 1
 print(f"{low_case_letters} маленьких букв в строке:")
 print(f"{upper_case_letters} больших букв в строке:")
