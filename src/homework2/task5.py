0d31cf16a536fa6177e28f077dc940f43dae7696
Ivan Veremeichik <43482831+NorgeMan@users.noreply.github.com>
2021-12-04 11:11:55 +0300

    Update task5.py

--- src/homework2/task5.py
+++ src/homework2/task5.py
@@ -1,10 +1,11 @@
 # task 5
 """Выведите n-ое число Фибоначчи, используя только временные
 переменные, циклические операторы и условные операторы. n - вводится"""
-# Числа Фибоначчи 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711
-number_of_fibonacci_raw = int(input("Введите номер числа из ряда: "))
-start_number = 0
+""" Числа Фибоначчи 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,
+233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711"""
+number_of_fibonacci_number = int(input("Введите номер числа из ряда: "))
+start = 0
 next_after_start_number = 1
-for number in range(1, number_of_fibonacci_raw):
-    start_number, next_after_start_number = next_after_start_number, start_number + next_after_start_number
-print(start_number)
+for number in range(1, number_of_fibonacci_number):
+    start, next_after_start_number = next_after_start_number, start + next_after_start_number
+print(start)
