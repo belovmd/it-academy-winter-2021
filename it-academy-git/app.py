c2fe1588e06bd93638358dd7a87b3af12ef9b241
MarinaIgorevna4 <marinatsykunenko97@gmail.com>
2021-12-15 22:10:02 +0300

    Input name and surname

--- it-academy-git/app.py
+++ it-academy-git/app.py
@@ -1,1 +1,4 @@
 print("I'm planning to learn git conflicts resolution")
+name = input('Please enter your name:')
+surname = input('Please enter your surname:')
+print('Hi, {} {}!'.format(name, surname))
