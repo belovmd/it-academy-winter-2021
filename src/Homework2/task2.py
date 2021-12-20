5ee1fbeb1a029fd8b1f4bcfd62333b5f786159dc
JIekceu <alexmashster@gmail.com>
2021-12-20 20:39:45 +0300

    Update task2.py

--- src/Homework2/task2.py
+++ src/Homework2/task2.py
@@ -9,11 +9,12 @@
 '''
 import re
 while True:
-    str_ = str(input("Введите строку: "))
-    str_2 = re.sub('[^ A-Za-z]', '', str_)
-    str_3 = str_2.split()
+    sentence = str(input("Введите строку: "))
+    sentence_cleared = re.sub('[^ A-Za-z]', '', sentence)
+    sentence_separated = sentence_cleared.split()
     ind_max = int()
-    for ind in range(len(str_3)):
-        if len(str_3[ind]) > len(str_3[ind_max]):
+    for ind in range(len(sentence_separated)):
+        if len(sentence_separated[ind]) > len(sentence_separated[ind_max]):
             ind_max = ind
-    print(str_3[ind_max])
+    print(sentence_separated[ind_max])
+
