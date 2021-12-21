7d0d58e5d57f0fe2200ba0a7cbec9a7cbed20b54
Andrey Sokolovsky <4sokky@gmail.com>
2021-12-21 23:32:05 +0300

    Homework2: Andrey Sokolovsky Did task 2 & 6

--- src/homework2/task2.py
+++ src/homework2/task2.py
@@ -2,3 +2,15 @@
 # Подсказки:
 # my_string.split([chars]) возвращает список строк.
 # len(list) - количество элементов в списке
+
+import string
+
+str_ = input("Введите строку: ").translate(str.maketrans("", "", string.punctuation))
+lst_ = str_.split()
+long_word = ""
+
+for word in lst_:   # А насколько правильно будет сразу проходить циклом по сплиту?
+    if len(word) > len(long_word):
+        long_word = word
+
+print(long_word)
--- src/homework2/task6.py
+++ src/homework2/task6.py
@@ -2,3 +2,17 @@
 # Число положительное целое, произвольной длины. Задача требует работать только с числами
 # (без конвертации числа в строку или что-нибудь еще)
 
+num = input("Введите число: ")
+num = int(num)
+copy_num = num
+result = 0
+
+while num != 0:
+    digit = num % 10
+    result = result * 10 + digit
+    num = int(num / 10)
+
+if result == copy_num:
+    print("{res} палиндром!".format(res=result))
+else:
+    print("{res} не палиндром!".format(res=result))
