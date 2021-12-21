0335cf6464400e5ed56df83c4c357b5115149cbd
Varabei Andrei <varabei1393@gmail.com>
2021-12-21 16:07:03 +0300

    task4

---
+++ src/homework3/task4.py
@@ -0,0 +1,12 @@
+# Дан список чисел. Посчитайте, сколько в нем пар элементов, равных друг другу.
+# Считается, что любые два элемента, равные друг другу образуют одну пару, которую необходимо посчитать.
+
+list_ = [3, 1, 1, 1, 1, 3, 3, 3]
+result = 0
+
+for elem in list_:
+    if list_.count(elem) > 1:
+        for i in range(list_.count(elem)):
+            list_.remove(elem)
+            result += list_.count(elem)
+print("Number of pairs:", result)
