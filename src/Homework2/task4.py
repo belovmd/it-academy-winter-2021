9a9ff635fc9e7d6ac7a11725a683aca03c98803d
Alex <94612947+JIekceu@users.noreply.github.com>
2021-12-23 18:43:15 +0300

    Update task4.py
    Added solution with dict

--- src/Homework2/task4.py
+++ src/Homework2/task4.py
@@ -1,4 +1,4 @@
-﻿'''
+'''
 4. Посчитать количество строчных (маленьких) и прописных (больших)
 букв в введенной строке. Учитывать только английские буквы.
 Задачу поместите в файл task4.py в папке src/homework2.
@@ -6,11 +6,18 @@
 '''
 propis = int()
 stroch = int()
-while True:
-    str_ = str(input("Введите строку: "))
-    for i in str_:
-        if 'A' <= i <= 'Z':
-            propis += 1
-        if 'a' <= i <= 'z':
-            stroch += 1
-    print(f'{propis} прописных букв, {stroch} строчных букв')
+str_ = str(input("Введите строку: "))
+for i in str_:
+    if 'A' <= i <= 'Z':
+        propis += 1
+    if 'a' <= i <= 'z':
+        stroch += 1
+print(f'{propis} прописных букв, {stroch} строчных букв')
+dict_ = {"propis": 0, "stroch": 0}
+str_ = str(input("Введите строку: "))
+for i in str_:
+    if 'A' <= i <= 'Z':
+        dict_["propis"] += 1
+    if 'a' <= i <= 'z':
+        dict_["stroch"] += 1      
+print(f'{dict_["propis"]} прописных букв, {dict_["stroch"]} строчных букв')
