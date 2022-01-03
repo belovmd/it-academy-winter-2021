9645cffe91060b340eadb27f59ca349c115100d6
Kazhe17 <alex.kozhemyachenok@gmail.com>
2022-01-03 21:14:36 +0300

    Revert "Homework2. Initial commit"
    This reverts commit 1749967abbd980c86c68196a89adcb53e244c58b.

--- src/homework2/Aleksandr Kozhemyachenok/task1.py
+++
@@ -1,4 +0,0 @@
-# m - number rub, n - number kop, s - number of things, l - result
-m, n, s = input('input m, n, s by space').split()
-L = int(s)*(int(m) * 100 + int(n))
-print(str(L // 100) + 'rub' + str(L % 100) + 'kop')
--- src/homework2/Aleksandr Kozhemyachenok/task3.py
+++
@@ -1,9 +0,0 @@
-str_ = input('input string...')
-result = ''
-str_.split()
-
-for el in str_:
-    if el not in result:
-        result = result + el
-
-print(result)
