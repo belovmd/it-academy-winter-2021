3ac20d2a72506ebe4091bb5a67434ba1fc98543f
Kazhe17 <alex.kozhemyachenok@gmail.com>
2022-01-03 22:15:18 +0300

    corrected task 8

--- src/homework2/Aleksandr Kozhemyachenok/task8.py
+++ src/homework2/Aleksandr Kozhemyachenok/task8.py
@@ -1,4 +1,5 @@
-""" Example_1(Elementary)
+"""
+Example_1(Elementary)
 You are given a sequence of strings.
 You should join these strings into a chunk of text where the initial
 strings are separated by commas.As a joke on the right handed robots,
@@ -19,8 +20,7 @@ if __name__ == "__main__":
 
     # These "asserts" using only for self-checking and not necessary for auto-testing
     assert (
-            left_join(("left", "right", "left", "stop")) == "left,left,left,stop"
-    ), "All to left"
+        left_join(("left", "right", "left", "stop")) == "left,left,left,stop"), "All to left"
     assert left_join(("bright aright", "ok")) == "bleft aleft,ok", "Bright Left"
     assert left_join(("brightness wright",)) == "bleftness wleft", "One phrase"
     assert left_join(("enough", "jokes")) == "enough,jokes", "Nothing to replace"
