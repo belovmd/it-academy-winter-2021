f849e1e625533d1e0c9c16cef3756cbccc4d7b9e
Varabei Andrei <varabei1393@gmail.com>
2022-01-12 15:07:31 +0300

    task5 update

--- src/honework4/task5.py
+++ src/honework4/task5.py
@@ -1,14 +1,14 @@
 # Каждый из N школьников некоторой школы знает Mi языков.
 # Определите, какие языки знают все школьники и языки, которые знает хотя бы один из школьников.
 
-n = int(input("Enter the number of students: "))
+number_students = int(input("Enter the number of students: "))
 result_1 = set()
 result_2 = set()
-for _ in range(n):
-    m = int(input("Enter nymber of languges the students know: "))
+for _ in range(number_students):
+    number_language = int(input("Enter number of languages the students know: "))
     set_ = set()
-    for elem in range(m):
-        set_.add(input("Enter languge: "))
+    for elem in range(number_language):
+        set_.add(input("Enter language: "))
     if result_1:
         result_1 = result_1 & set_
     else:
@@ -17,10 +17,10 @@ for _ in range(n):
 
 len_1 = len(result_1)
 len_2 = len(result_2)
-print("Number of languges that all students know: ", len_1)
+print("Number of languages that all students know: ", len_1)
 for _ in range(len_1):
     print(result_1.pop())
 
-print("Number of languges that at least one student know: ", len_2)
+print("Number of languages that at least one student know: ", len_2)
 for _ in range(len_2):
     print(result_2.pop())
