0a97caddfde2448cd2c770f9f2b9ae5aaf446426
Krotiks <94705794+Krotiks@users.noreply.github.com>
2022-01-13 23:36:15 +0300

    Update task4.py

--- src/task4.py
+++ src/task4.py
@@ -1,11 +1,11 @@
-a = int(input("Введите число:"))
-number = a
-rev = 0
-while a > 0:
-    dig = a % 10
-    rev = rev * 10 + dig
-    a = a // 10
-if(number == rev):
-    print("Это палиндром!")
-else:
-    print("Это не палиндром!")
+string = input('Ведите строку: ')
+small_l = 0
+big_l = 0
+for el in string:
+    if 'a' <= el <= 'z':
+        small_l += 1
+    else:
+        if 'A' <= el <= 'Z':
+            big_l += 1
+print('Строчных букв:', small_l)
+print('Прописных букв:', big_l)
