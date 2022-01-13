45fedd454540f1b706664d31fe71c39b914b3e68
Andrey Sokolovsky <4sokky@gmail.com>
2022-01-13 21:35:37 +0300

    Homework3: Andrey Sokolovsky Fixes for circleci tests Did task6.py

--- src/homework3/task5.py
+++ src/homework3/task5.py
@@ -2,16 +2,10 @@
 # Дан список. Выведите те его элементы, которые встречаются в списке только один раз.
 # Элементы нужно выводить в том порядке, в котором они встречаются в списке.
 
-# Если список дан изначально:
-# lst = ["abc", 1, 2, 2, 3, 4, 5, 3, "elem", "abc"]
-# Либо, если список составляет пользователь:
-lst_quantity = int(input("Введите количество элементов в списке: "))
-lst = []
-for lst_obj in range(lst_quantity):
-    lst.append(input('Введите объект списка: '))
+lst_ = ["abc", 1, 2, 2, 3, 4, 5, 3, "elem", "abc"]
 
 lst_result = []
-for elem in lst:
+for elem in lst_:
     if elem not in lst_result:
         lst_result.append(elem)
 
