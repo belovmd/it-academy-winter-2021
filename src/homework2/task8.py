d7a50caf46deecddbe4ef89e0e84385fc97074ae
Andrey Sokolovsky <4sokky@gmail.com>
2022-01-13 20:07:05 +0300

    Homework2: Andrey Sokolovsky Fixes for circleci tests

--- src/homework2/task8.py
+++ src/homework2/task8.py
@@ -1,3 +1,5 @@
 # 8. Зарегистрируйтесь на одном (или нескольких) из сайтов:
-# https://py.checkio.org/ , https://www.codewars.com,   https://www.hackerrank.com/, https://acmp.ru
-# И решите 1-5 задач уровня Elementary и advanced. Поместите 3 простых и 2 сложных задачи на Ваш выбор в пул реквест.
+# https://py.checkio.org/ , https://www.codewars.com, https://www.hackerrank.com/, https://acmp.ru
+# И решите 1-5 задач уровня Elementary и advanced.
+# Поместите 3 простых и 2 сложных задачи на Ваш выбор в пул реквест.
+
