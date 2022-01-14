cee32b93709ddf7cc2f86d6560b4fd3b35f910d2
Varabei Andrei <varabei1393@gmail.com>
2022-01-14 13:13:38 +0300

    task7 update

--- src/honework4/task7.py
+++ src/honework4/task7.py
@@ -2,15 +2,14 @@
 # Вычислите их наибольший общий делитель при помощи алгоритма Евклида
 # (мы не знаем функции и рекурсию).
 
-first_number = 4
-second_number = 18
+first_number = 15
+second_number = 2
 list_ = []
 
 if first_number and second_number:
     list_.append(first_number)
     list_.append(second_number)
-    list_.sort()
-    list_.reverse()
+    list_.sort(reverse = True)
 
     while list_[-1]:
         list_.append(list_[-2] % list_[-1])
