bcccd9b82e9dbfcca4f57bbefd2c73987aa5e691
Varabei Andrei <varabei1393@gmail.com>
2022-01-14 13:17:53 +0300

    check issues eflake8

--- src/honework4/task7.py
+++ src/honework4/task7.py
@@ -3,17 +3,17 @@
 # (мы не знаем функции и рекурсию).
 
 first_number = 15
-second_number = 2
+second_number = 5
 list_ = []
 
 if first_number and second_number:
     list_.append(first_number)
     list_.append(second_number)
-    list_.sort(reverse = True)
+    list_.sort(reverse=True)
 
     while list_[-1]:
         list_.append(list_[-2] % list_[-1])
 
-    print("Max divide:",list_[-2])
+    print("Greatest max divisor:", list_[-2])
 else:
     print("Integer must be more than zero")
