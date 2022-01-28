e9236947ab99933e3bd3bd78dc5f9facaa831edf
Varabei Andrei <varabei1393@gmail.com>
2022-01-28 15:38:28 +0300

    fixies after code review

--- src/Homework2/task3.py
+++ src/Homework2/task3.py
@@ -5,9 +5,7 @@ str_without_space = input_str.replace(" ", "")
 output_str = ""
 
 for elem in str_without_space:
-    if elem in output_str:
-        continue
-    else:
+    if not elem in output_str:
         output_str += elem
 
 print(output_str)
--- src/Homework2/task5.py
+++ src/Homework2/task5.py
@@ -1,19 +1,11 @@
 # Вывод n-го число Фибоначчи
 
 n = int(input("Введите n число Фибоначи: "))
+
 fib_1 = 0
 fib_2 = 1
-fib_n = 0
 
-if n > 0:
-    for counter in range(0, n):
-        fib_n = fib_1 + fib_2
-        fib_2 = fib_1
-        fib_1 = fib_n
-else:
-    for counter in range(n, 0):
-        fib_n = fib_2 - fib_1
-        fib_2 = fib_1
-        fib_1 = fib_n
+for elem in range(0, n):
+    fib_1, fib_2 = fib_2, fib_1 + fib_2
 
 print("n число Фибоначи равно: ", fib_1)
