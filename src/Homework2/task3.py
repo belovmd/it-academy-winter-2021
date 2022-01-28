81c4b9b363515248239176d0c4a34cb171952487
Varabei Andrei <varabei1393@gmail.com>
2022-01-28 15:51:53 +0300

    task update

--- src/Homework2/task3.py
+++ src/Homework2/task3.py
@@ -5,7 +5,7 @@ str_without_space = input_str.replace(" ", "")
 output_str = ""
 
 for elem in str_without_space:
-    if not elem in output_str:
+    if elem not in output_str:
         output_str += elem
 
 print(output_str)
