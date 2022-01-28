35cec0690b96f433d50f93a19cc8fb192f899eb9
Varabei Andrei <varabei1393@gmail.com>
2022-01-28 16:32:11 +0300

    task5 update

--- src/Homework2/task5.py
+++ src/Homework2/task5.py
@@ -5,7 +5,7 @@ n = int(input("Введите n число Фибоначи: "))
 fib_1 = 0
 fib_2 = 1
 
-for elem in range(0, n):
+for elem in range(n):
     fib_1, fib_2 = fib_2, fib_1 + fib_2
 
 print("n число Фибоначи равно: ", fib_1)
