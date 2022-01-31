eea77e67f3bd02d58590ed7eba53b6ec4cac8c6f
Alex <alexmashster@gmail.com>
2022-01-31 21:21:38 +0300

    fixed task1

--- src/Homework3/task1.py
+++ src/Homework3/task1.py
@@ -10,6 +10,6 @@ for i in range(1, 101):
         prn = 'Fizz'
     if i % 5 == 0:
         prn = 'Buzz'
-    if i % 3 == 0 and i % 5 == 0:
+    if i % 15 == 0:
         prn = 'FizzBuzz'
     print(prn)
