986710ad1164b262c459554227ef94dff100c97a
Roman Snetkov <romajmg@gmail.com>
2022-01-31 15:26:37 +0300

    First 3 tasks

---
+++ src/task1.py
@@ -0,0 +1,28 @@
+"""Write a program that prints numbers from 1 to 100.
+
+But instead of numbers divisible by 3 it writes Fizz, instead of numbers divisible by 5 it writes Buzz, and instead of numbers simultaneously divisible by both 3 and 5 it writes FizzBuzz.
+"""
+
+for _ in range(101):
+    if _ % 3 and _ % 5:
+        print(_)
+    elif _ % 5:
+        print('Fizz')
+    elif _ % 3:
+        print('Buzz')
+    else:
+        print('FizzBuzz')
+
+
+
+""" The one below is less readeable, but for me is more logical"""
+
+# for _ in range(101):
+#     if not _ % 3  and not _ % 5:
+#         print('FizzBuzz')
+#     elif not _ % 3:
+#         print('Fizz')
+#     elif not _ % 5:
+#         print('Buzz')
+#     else:
+#         print(_)
