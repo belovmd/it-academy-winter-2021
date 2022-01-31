b8be78b1ff6420581d74d608735098eb291fcd11
Alex <alexmashster@gmail.com>
2022-01-31 21:04:15 +0300

    fixed task6

--- src/Homework2/task6.py
+++ src/Homework2/task6.py
@@ -1,14 +1,19 @@
-import copy
+'''
+6. Определите, является ли число палиндромом
+(читается слева направо и справа налево одинаково).
+Число положительное целое, произвольной длины.
+Задача требует работать только с числами
+(без конвертации числа в строку или что-нибудь еще)
+Задачу поместите в файл task6.py в папке src/homework2.
+'''
 while True:
-    chislo = int(input('Введите число: '))
-    lst_ = []
-    lst_rev = []
+    chislo_in = int(input('Введите число: '))
+    chislo = chislo_in
+    chislo_rev = int()
     while chislo > 0:
-        lst_.append(chislo % 10)
+        chislo_rev = chislo_rev * 10 + chislo % 10
         chislo = chislo // 10
-    lst_rev = copy.deepcopy(lst_)
-    lst_rev.reverse()
-    if lst_ == lst_rev:
+    if chislo_in == chislo_rev:
         print('Это палиндром')
     else:
         print('Это не палиндром')
