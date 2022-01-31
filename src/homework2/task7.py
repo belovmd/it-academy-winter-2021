859e2a6c09c6678c98605b438720a9783529b9a7
Roman Snetkov <romajmg@gmail.com>
2022-01-31 21:16:34 +0300

    Fixed flake8

--- src/homework2/task7.py
+++ src/homework2/task7.py
@@ -9,9 +9,9 @@ If not, display an invalid data message.
 input_ = input('Input triangle sides separately: ').split()
 side1, side2, side3 = (int(value) for value in input_)
 
-if ((side1 < side2 + side3) and
-        (side2 < side1 + side3) and
-        (side3 < side1 + side2)):
+if ((side1 < side2 + side3)
+        and (side2 < side1 + side3)
+        and (side3 < side1 + side2)):
     p = (side1 + side2 + side3) / 2
     area = (p * (p - side1) * (p - side2) * (p - side3)) ** 0.5
     print('It is triangle. Area:', area)
