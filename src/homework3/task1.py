65d475c170747fab9006d8100101c7092fa27391
Andrey Sokolovsky <4sokky@gmail.com>
2022-01-31 21:19:38 +0300

    Homework3: Andrey Sokolovsky Fixed task 1 after review (final)

--- src/homework3/task1.py
+++ src/homework3/task1.py
@@ -2,12 +2,12 @@
 # Напишите программу, которая печатает цифры от 1 до 100, но вместо чисел, кратных 3 пишет Fizz,
 # вместо чисел кратный 5 пишет Buzz, а вместо чисел одновременно кратных и 3 и 5 - FizzBuzz
 
-for elem in range(1, 101):
-    if not elem % 3 and not elem % 5:
+for digit in range(1, 101):
+    if not digit % 15:
         print("FizzBuzz")
-    elif not elem % 3:
+    elif not digit % 3:
         print("Fizz")
-    elif not elem % 5:
+    elif not digit % 5:
         print("Buzz")
     else:
-        print(elem)
+        print(digit)
