8472c62a54e59921f20c81ee5c9319747408d0d5
Varabei Andrei <varabei1393@gmail.com>
2022-02-02 12:54:17 +0300

    fix eflake8

--- src/Homework2/task 8_5.py
+++ src/Homework2/task 8_5.py
@@ -7,7 +7,7 @@ list_.sort()
 result = 0
 
 if list_[0] != list_[1]:
-    for elem in range(list_[0],list_[1]+1):
+    for elem in range(list_[0], list_[1] + 1):
         result += elem
 else:
     result = list_[0]
