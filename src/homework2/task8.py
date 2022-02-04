545957242f73a08b623757e25c111224f7ace199
Andrey Sokolovsky <4sokky@gmail.com>
2022-02-04 19:42:14 +0300

    Homework2: Andrey Sokolovsky Added smthg in task8.py

--- src/homework2/task8.py
+++ src/homework2/task8.py
@@ -3,12 +3,45 @@
 # И решите 1-5 задач уровня Elementary и advanced.
 # Поместите 3 простых и 2 сложных задачи на Ваш выбор в пул реквест.
 
+
 # codewars.com, elementary #1
 # Remove all of the vowels from string
 # Note: for this kata "y" isn't considered a vowel.
 def disemvowel(string_):
     for elem in string_:
         if elem in "aoeuiAOEUI":
-            str_ = string_.replace(elem, "")
-            string_ = str_
+            string_ = string_.replace(elem, "")
     return string_
+
+
+# hackerrank.com, elementary #2
+# The provided code stub reads and integer, n, from STDIN.
+# For all non-negative integers i < n, print i in square.
+if __name__ == '__main__':
+    n = int(raw_input())
+
+if n:
+    for elem in range(0, n):
+        print(elem ** 2)
+
+
+# hackerrank.com, elementary #3
+# Given a year, determine whether it is a leap year.
+# If it is a leap year, return the Boolean True, otherwise return False.
+# Note that the code stub provided reads from STDIN and passes arguments to the is_leap function.
+# It is only necessary to complete the is_leap function.
+# In the Gregorian calendar, three conditions are used to identify leap years:
+#   The year can be evenly divided by 4, is a leap year, unless:
+#       The year can be evenly divided by 100, it is NOT a leap year, unless:
+#           The year is also evenly divisible by 400. Then it is a leap year.
+def is_leap(year):
+    leap = False
+
+    if (not year % 4) and (year % 100) or (not year % 400):
+        leap = True
+
+    return leap
+
+
+year = int(raw_input())
+print(is_leap(year))
