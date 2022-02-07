424b12a4fd451d05a6c40462335e43fa179df8ba
Varabei Andrei <varabei1393@gmail.com>
2022-02-07 18:40:22 +0300

    task2

--- src/task2.py
+++ src/task2.py
@@ -1,23 +1,32 @@
 # Создайте декоратор, который хранит результаты вызовов функции
 # (за все время вызовов, не только текущий запуск программы)
 
+import time
+
+
 def dec(func):
-    result = []
 
-    def wrapper():
-        nonlocal result
-        result.append(func())
-        return result
+    def wrapper(*args, **kwargs):
 
+        write = "Function {}, Time {}, Result {}, Variables {} "
+        with open("result_task2", 'a') as fh:
+            fh.write(write.format(func.__name__, time.asctime(), func(*args), args) + "\n")
+        return func
     return wrapper
 
 
 @dec
-def func():
-    return 1
+def func(b):
+    return b
 
+@dec
+def func_two(a):
+    return a
 
-print(func())
-print(func())
-print(func())
-print(func())
+@dec
+def func_three(a, b, c):
+    return a + b + c
+
+func("bbb")
+func_two(7)
+func_three("a", "124", "mmm")
