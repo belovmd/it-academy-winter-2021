63f5c8794ebdbf88d8181bd674cf5aae0fb59e7a
Varabei Andrei <varabei1393@gmail.com>
2022-02-08 18:28:57 +0300

    fix issues eflake8

--- src/task5.py
+++ src/task5.py
@@ -1,10 +1,10 @@
-#Написать программу которая находит ближайшую степень двойки к введенному числу.
+# Написать программу которая находит ближайшую степень двойки к введенному числу.
 # 10(8), 20(16), 1(1), 13(16)
 
 number = int(input("Enter your number: "))
 bin_number = format(number, "b")
 
-result_1 = 2 ** (len(bin_number)-1)
+result_1 = 2 ** (len(bin_number) - 1)
 result_2 = 2 ** (len(bin_number))
 
 if abs(result_1 - number) < abs(result_2 - number):
