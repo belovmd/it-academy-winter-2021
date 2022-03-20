e0eb40970ab1efa1221000bd48969f48c9ecc28c
Andrey Sokolovsky <4sokky@gmail.com>
2022-03-20 15:27:35 +0300

    Homework4: Andrey Sokolovsky Did tasks 2, 5

--- src/homework4/task2.py
+++ src/homework4/task2.py
@@ -1,10 +1,24 @@
 # 2. Города
 # Дан список стран и городов каждой страны. Затем даны названия городов.
 # Для каждого города укажите, в какой стране он находится.
-# Входные данные
-# Программа получает на вход количество стран N. Далее идет N строк,
-# каждая строка начинается с названия страны, затем идут названия городов этой страны.
-# В следующей строке записано число M, далее идут M запросов — названия каких-то M городов, перечисленных выше.
-# Выходные данные
-# Для каждого из запроса выведите название страны, в котором находится данный город.
 
+import string
+
+countries_amount = int(input("Введите количество стран: "))
+countries_cities = {}
+
+for countries in range(1, countries_amount + 1):
+    input_countries = input("Введите название {n}-й страны и её города: ".format(n=countries)
+                            ).translate(str.maketrans("", "", string.punctuation)).split()
+    countries_cities[input_countries[0]] = input_countries[1:]
+
+cities_amount = int(input("Введите количество городов: "))
+cities = []
+
+for city in range(1, cities_amount + 1):
+    cities.append(input("Введите название {n}-го города: ".format(n=city)))
+
+for city2 in cities:
+    for country in countries_cities:
+        if city2 in countries_cities[country]:
+            print("{country}: {city}".format(country=country, city=city2))
--- src/homework4/task5.py
+++ src/homework4/task5.py
@@ -1,8 +1,30 @@
 # 5. Языки
 # Каждый из N школьников некоторой школы знает Mi языков.
 # Определите, какие языки знают все школьники и языки, которые знает хотя бы один из школьников.
-# Входные данные
-# Первая строка входных данных содержит количество школьников N.
-# Далее идет N чисел Mi, после каждого из чисел идет Mi строк,
-# содержащих названия языков, которые знает i-й школьник.
 
+students_amount = int(input("Введите количество школьников: "))
+students_langs = []
+
+for student in range(1, students_amount + 1):
+    langs_amount = int(input("Введите количество языков для {s}-го школьника: ".format(s=student)))
+    langs = set()
+
+    for lang in range(1, langs_amount + 1):
+        language = input("Введите {l}-й язык для {s}-го школьника: ".format(l=lang, s=student))
+        langs.add(language)
+
+    students_langs.append(langs)
+
+all_knows = students_langs[0]
+any_knows = set()
+
+for num in range(students_amount):
+    all_knows &= students_langs[num]
+    any_knows |= students_langs[num]
+
+print("Все школьники знают {n} язык(-а/ов):".format(n=len(all_knows)))
+for elem in all_knows:
+    print(elem)
+print("Хотя бы один из школьников знает 1 из {n} язык(-а/ов):".format(n=len(any_knows)))
+for elem2 in any_knows:
+    print(elem2)
