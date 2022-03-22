2e807742f9d13011e22c30a9e9f6f9f9bde5a436
Andrey Sokolovsky <4sokky@gmail.com>
2022-03-22 12:36:51 +0300

    Homework5: Andrey Sokolovsky Did tasks 5, 6, modify task 3

--- src/homework5/task3.py
+++ src/homework5/task3.py
@@ -5,27 +5,33 @@
 #  get_ranges([2, 3, 8, 9]) // "2-3,8-9"
 
 def get_ranges(list_):
-    first_num = last_num = 0
-    dict_ = {}
-    result = ""
+    if list_:
+        list_ = sorted(list_)
+        first_num = last_num = 0
+        dict_ = {}
 
-    for num in list_:
-        if num == last_num + 1:
-            last_num = num
-            dict_[first_num] = dict_.get(first_num, 0) + 1
-        else:
-            dict_.setdefault(num, 0)
-            first_num = last_num = num
+        for num in list_:
+            if num == last_num + 1:
+                last_num = num
+                dict_[first_num] = dict_.get(first_num, 0) + 1
+            else:
+                dict_.setdefault(num, 0)
+                first_num = last_num = num
 
-    for key, value in dict_.items():
-        if value:
-            result += "{first}-{last},".format(first=key, last=key+value)
-        else:
-            result += "{first},".format(first=key)
+        result = ""
 
-    return result.strip(",")
+        for key, value in dict_.items():
+            if value:
+                result += "{first}-{last},".format(first=key, last=key+value)
+            else:
+                result += "{first},".format(first=key)
+
+        return result.strip(",")
+    else:
+        return "Список пуст"
 
 
+print(get_ranges([]))
 print(get_ranges([0, 1, 2, 3, 4, 7, 8, 10]))
 print(get_ranges([4, 7, 10]))
 print(get_ranges([2, 3, 8, 9]))
--- src/homework5/task5.py
+++ src/homework5/task5.py
@@ -1,3 +1,14 @@
 # 5. Написать программу которая находит ближайшую степень двойки к введенному числу.
+# 10(8), 20(16), 1(1), 13(16)
 
+num = int(input("Введите число: "))
+degree = 1
 
+if not num % 2 and num > 0:
+    while degree < (num / 2):
+        degree = degree * 2     # или degree << 1, но я разницу не увидел при подсчёте
+elif num % 2 and num > 0:       # или просто что-то неправильно сделал
+    while degree < num:
+        degree = degree * 2
+
+print("Ближайшая степень двойки к числу {n}: {d}".format(n=num, d=degree))
