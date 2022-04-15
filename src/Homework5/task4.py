b7d827412c1bb3bda675c7030863a7e50add5a87
Alex <alexmashster@gmail.com>
2022-04-15 18:53:21 +0300

    Added txt histograms to task4, added graphics files

--- src/Homework5/task4.py
+++ src/Homework5/task4.py
@@ -25,10 +25,10 @@ list_rank = []
 list_year = []
 list_str_array_year = []
 dict_rank = dict
-
 set_year = set()
 dict_year = dict
 y_list_year = []
+
 try:
     open('ratings.list', 'rt')
 except FileNotFoundError:
@@ -46,54 +46,65 @@ else:
         list_title.pop()
         list_rank.pop()
         list_year.pop()
-        #        print(list_title)
-        #        print(list_rank)
-        #        print(list_year)
 
         file1 = open('top250_movies.txt', 'w')
         file1.writelines(line + '\n' for line in list_title)
         file1.close()
+        print('Export titles complete')
+
+        list_array_rank = []
+        x_rank = np.arange(float(min(list_rank)), (float(max(list_rank)) + 0.1), 0.1)
+        print(x_rank)
+        for elem in x_rank:
+            list_array_rank.append(round(float(elem), 1))
+
+        float_x = []
+        list_rank_y = []
+        for elem in x_rank:
+            float_x.append(str(round(elem, 1)))
+        dict_rank = {key: 0 for key in float_x}
+        for key in dict_rank:
+            dict_rank[key] = list_rank.count(key)
+        for _ in dict_rank.values():
+            list_rank_y.append(_)
+        y_rank = list_rank_y
+        exp_rank = []
+        for i in range(len(x_rank)):
+            exp_rank.append(str(list_array_rank[i])+': '+(str(y_rank[i]*'*')))
         file2 = open('ratings.txt', 'w')
-        file2.writelines(line + '\n' for line in list_rank)
+        file2.writelines(line + '\n' for line in exp_rank)
         file2.close()
+        print('Export rates complete')
+
+        fig1, ax = plt.subplots()
+        ax.bar(x_rank, y_rank)
+        ax.set_facecolor('seashell')
+        fig1.set_facecolor('floralwhite')
+        fig1.set_figwidth(12)  # ширина Figure
+        fig1.set_figheight(6)  # высота Figure
+
+        x_year = np.arange(int(min(list_year)), (int(max(list_year)) + 1), 1)
+        for elem in x_year:
+            list_str_array_year.append(str(elem))
+        dict_year = {key: 0 for key in list_str_array_year}
+        for key in dict_year:
+            dict_year[key] = list_year.count(key)
+        for _ in dict_year.values():
+            y_list_year.append(_)
+
+        exp_year = []
+        for i in range(len(x_year)):
+            exp_year.append(str(list_str_array_year[i]) + ': ' + (str(y_list_year[i] * '*')))
+
         file3 = open('years.txt', 'w')
-        file3.writelines(line + '\n' for line in list_year)
+        file3.writelines(line + '\n' for line in exp_year)
         file3.close()
-        print('Export complete')
+        print('Export years complete')
 
-    x_rank = np.arange(float(min(list_rank)), (float(max(list_rank)) + 0.1), 0.1)
-    float_x = []
-    list_rank_y = []
-    for elem in x_rank:
-        float_x.append(str(round(elem, 1)))
-    dict_rank = {key: 0 for key in float_x}
-    for key in dict_rank:
-        dict_rank[key] = list_rank.count(key)
-    for _ in dict_rank.values():
-        list_rank_y.append(_)
-    y_rank = list_rank_y
-
-    fig1, ax = plt.subplots()
-    ax.bar(x_rank, y_rank)
-    ax.set_facecolor('seashell')
-    fig1.set_facecolor('floralwhite')
-    fig1.set_figwidth(12)  # ширина Figure
-    fig1.set_figheight(6)  # высота Figure
-    plt.show()
-
-    x_year = np.arange(int(min(list_year)), (int(max(list_year)) + 1), 1)
-    for elem in x_year:
-        list_str_array_year.append(str(elem))
-    dict_year = {key: 0 for key in list_str_array_year}
-    for key in dict_year:
-        dict_year[key] = list_year.count(key)
-    for _ in dict_year.values():
-        y_list_year.append(_)
-
-    fig2, bx = plt.subplots()
-    bx.bar(x_year, y_list_year)
-    bx.set_facecolor('seashell')
-    fig2.set_facecolor('floralwhite')
-    fig2.set_figwidth(12)  # ширина Figure
-    fig2.set_figheight(6)  # высота Figure
-    plt.show()
+        fig2, bx = plt.subplots()
+        bx.bar(x_year, y_list_year)
+        bx.set_facecolor('seashell')
+        fig2.set_facecolor('floralwhite')
+        fig2.set_figwidth(12)  # ширина Figure
+        fig2.set_figheight(6)  # высота Figure
+        plt.show()
