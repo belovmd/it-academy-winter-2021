26b97b4c0f28ae018fc187c3deff195144103ed2
Kazhe17 <alex.kozhemyachenok@gmail.com>
2022-04-17 20:29:46 +0300

    Initial commit: Homework 5

---
+++ src/homework5/task5.py
@@ -0,0 +1,13 @@
+# Написать программу которая находит ближайшую степень
+# к введенному числу
+
+
+num = int(input("Enter number: "))
+b_num = f'{num:b}'
+result_1 = 2 ** (len(b_num))
+result_2 = 2 ** (len(b_num) - 1)
+
+if abs(result_1 - num) < abs(result_2 - num):
+    print('two in power : ', result_1)
+else:
+    print('two in power : ', result_2)
